36cb731392c58fab7cd790103254b7bef17eaa13
Agustin <perezp.agustin@gmail.com>
2024-04-13 13:25:03 -0300

    fix: add validations

--- part3/phonebookApi/index.js
+++ part3/phonebookApi/index.js
@@ -1,7 +1,16 @@
-let { persons } = require('./persons')
-
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+
+let { persons } = require('./persons')
+
+const generateId = () => Math.floor(Math.random() * 1000)
+
+const isNameDuplicated = (name) => {
+  if (!persons?.length) return false
+  return persons?.find(p => p?.name?.toLowerCase() === name.toLowerCase())
+}
 
 app.get('/api/info', (request, response) => {
   response.send(`
@@ -26,6 +35,28 @@ app.delete('/api/persons/:id', (request, response) => {
   const personId = Number(request?.params?.id)
   persons = persons?.filter(({ id }) => personId !== id)
   response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request?.body
+
+  if (!body?.name || !body?.number) {
+    return response.status(400).send('Params missing')
+  }
+
+  if (isNameDuplicated(body.name)) {
+    return response.status(400).send('Name must be unique')
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons?.concat(person)
+  
+  response.json(person)
 })
 
 const PORT = 3001
